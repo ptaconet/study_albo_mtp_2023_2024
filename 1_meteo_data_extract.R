f54156353d6ff7a2c6f8c02f44c5bda997aed61f
Paul Taconet <paul.taconet@ird.fr>
2024-07-18 17:04:58 +0200

    upload script to extract meteofrance data

---
+++ 1_meteo_data_extract.R
@@ -0,0 +1,19 @@
+library(httr)
+
+fun_extract_meteo_data <- function(month,year,path){
+
+  data <- paste0("synop.",as.character(year),as.character(month),".csv.gz")
+  url <- paste0("https://donneespubliques.meteofrance.fr/donnees_libres/Txt/Synop/Archive/",data)
+
+  httr::GET(url,httr::write_disk(file.path(path,data)),httr::progress(),config = list(maxredirs=-1))
+
+}
+
+months <- c("01","02","03","04","05","06","07","08","09","10","11","12")
+years <- c("2023","2024")
+
+for(i in 1:length(years)){
+  for(j in 1:length(months)){
+    fun_extract_meteo_data(months[j],years[i],"data_meteofrance")
+  }
+}
